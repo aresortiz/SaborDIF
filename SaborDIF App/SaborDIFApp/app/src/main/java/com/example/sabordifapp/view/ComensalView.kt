d16c12b617f62f09f7f424158e1beec70a00791b
Ares Ortiz <aortizb597@gmail.com>
2023-10-15 14:45:56 -0600

    Escanear QR e inserción de datos (comensal)

--- SaborDIF App/SaborDIFApp/app/src/main/java/com/example/sabordifapp/view/ComensalView.kt
+++ SaborDIF App/SaborDIFApp/app/src/main/java/com/example/sabordifapp/view/ComensalView.kt
@@ -2,6 +2,10 @@ package com.example.sabordifapp.view
 
 import android.content.Context
 import android.os.Bundle
+import android.print.PrintJobInfo.STATE_CANCELED
+import android.print.PrintJobInfo.STATE_COMPLETED
+import android.print.PrintJobInfo.STATE_FAILED
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -17,6 +21,10 @@ import com.example.sabordifapp.model.API.condicion.RegistroCondicion
 import com.example.sabordifapp.viewmodel.APIVM.viewmodel.ComensalVM
 import com.example.sabordifapp.viewmodel.APIVM.viewmodel.CondicionVM
 import com.example.sabordifapp.viewmodel.ComensalViewModel
+import com.google.android.gms.common.moduleinstall.InstallStatusListener
+import com.google.android.gms.common.moduleinstall.ModuleInstall
+import com.google.android.gms.common.moduleinstall.ModuleInstallRequest
+import com.google.android.gms.common.moduleinstall.ModuleInstallStatusUpdate
 import com.google.mlkit.vision.codescanner.GmsBarcodeScanning
 
 class ComensalView : Fragment() {
@@ -39,6 +47,7 @@ class ComensalView : Fragment() {
         return binding.root
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         registrarEventos()
@@ -51,6 +60,7 @@ class ComensalView : Fragment() {
                 mapaCondiciones[objetoCondicion.nombreCondicion] = objetoCondicion.IdCondicion
             }
             val listaCondiciones = ArrayList(mapaCondiciones.keys)
+            listaCondiciones.add(0, "Ninguna/No aplica")
             binding.spnCondicion.adapter = ArrayAdapter(
                 requireContext(),
                 android.R.layout.simple_spinner_item,
@@ -71,28 +81,8 @@ class ComensalView : Fragment() {
 
     private fun registrarEventos() {
 
-        val escanearQR = GmsBarcodeScanning.getClient(requireContext())
-        binding.btnescanearQR.setOnClickListener() {
-            escanearQR.startScan()
-                .addOnSuccessListener { barcode ->
-                    val rawValue: String? = barcode.rawValue
-                    val array = rawValue.toString().split("||")
-                    val curp = array[0]
-                    val array2 = array[1].split("|")
-                    val apellidoP = array2[0]
-                    val apellidoM = array2[1]
-                    val nombreC = array2[2]
-                    val genero = array2[3]
-                    binding.inputCURP.setText(curp)
-                    binding.inputApellidoP.setText(apellidoP)
-                    binding.inputApellidoM.setText(apellidoM)
-                    binding.inputNombre.setText(nombreC)
-                    binding.inputGenero.setText(genero)
-                }.addOnCanceledListener {
-                    //Task cancelled
-                }.addOnFailureListener{e ->
-                    //Task failed with an exception
-                }
+        verificarQR()
+        escanearQR()
 
             binding.btnRegistrar.setOnClickListener {
 
@@ -117,19 +107,23 @@ class ComensalView : Fragment() {
                         val editor = sharedPref.edit()
                         editor.putString("IDComensal", comensalId.idComensal.toString())
                         editor.apply()
-                        val idCondicion = mapaCondiciones.get(condicion)
+                        if(condicion != "Ninguna/No aplica"){
+                            val idCondicion = mapaCondiciones.get(condicion)
 
-                        if (idCondicion != null) {
+                            if (idCondicion != null) {
 
-                            val condicion = CondicionVM()
-                            val listaCondiciones = listOf<RegistroCondicion>(
-                                RegistroCondicion(
-                                    comensalId.idComensal,
-                                    idCondicion
+                                val condicion = CondicionVM()
+                                val listaCondiciones = listOf<RegistroCondicion>(
+                                    RegistroCondicion(
+                                        comensalId.idComensal,
+                                        idCondicion
+                                    )
                                 )
-                            )
-                            condicion.registrarCondicion(listaCondiciones)
+                                condicion.registrarCondicion(listaCondiciones)
+                            }
+
                         }
+
                     }
                 }
                 val accion = ComensalViewDirections.actionComensalToRegistro()
@@ -137,5 +131,109 @@ class ComensalView : Fragment() {
             }
         }
 
+    private fun escanearQR()
+    {
+        Log.d("API_TEST", "Inicia QR")
+        val escanearQR = GmsBarcodeScanning.getClient(requireContext())
+        binding.btnescanearQR.setOnClickListener() {
+            escanearQR.startScan()
+                .addOnSuccessListener { barcode ->
+                    val rawValue: String? = barcode.rawValue
+                    val array = rawValue.toString().split("|")
+                    val curp = array[0]
+                    val apellidoP = array[2]
+                    val apellidoM = array[3]
+                    val nombreC = array[4]
+                    val genero = array[5]
+                    Log.d("API_TEST", "Este es el raw value ${rawValue}")
+                    binding.inputCURP.setText(curp)
+                    binding.inputApellidoP.setText(apellidoP)
+                    binding.inputApellidoM.setText(apellidoM)
+                    binding.inputNombre.setText(nombreC)
+                    binding.inputGenero.setText(genero)
+                }.addOnCanceledListener {
+                    //Task cancelled
+                }.addOnFailureListener { e ->
+                    //Task failed with an exception
+                }
+        }
+
     }
+
+    private fun verificarQR()
+    {
+
+        val moduleInstallClient = ModuleInstall.getClient(requireContext())
+        val optionalModuleApi = GmsBarcodeScanning.getClient(requireContext())
+
+        moduleInstallClient
+            .areModulesAvailable(optionalModuleApi)
+            .addOnSuccessListener {
+                if (it.areModulesAvailable()) {
+                    println("<Si está instalado")
+                    escanearQR()
+                } else {
+                    println("No está instalado")
+                    instalarModulo()
+                }
+            }
+            .addOnFailureListener {
+                println("Error al verificar módulo")
+            }
+
+    }
+
+    private fun instalarModulo() {
+        val moduleInstallClient = ModuleInstall.getClient(requireContext())
+        val optionalModuleApi = GmsBarcodeScanning.getClient(requireContext())
+
+        val moduleInstallRequest =
+            ModuleInstallRequest.newBuilder()
+                .addApi(optionalModuleApi)
+                .setListener(listener)
+                .build()
+
+        moduleInstallClient
+            .installModules(moduleInstallRequest)
+            .addOnSuccessListener {
+                if (it.areModulesAlreadyInstalled()) {
+                    // Modules are already installed when the request is sent.
+                    println("I N S T A L A N D O ......")
+                }
+            }
+            .addOnFailureListener {
+                println("NO SE PUEDE INSTALAR")
+
+            }
+    }
+
+    inner class ModuleInstallProgressListener : InstallStatusListener {
+        override fun onInstallStatusUpdated(update: ModuleInstallStatusUpdate) {
+            // Progress info is only set when modules are in the progress of downloading.
+            update.progressInfo?.let {
+                val progress = (it.bytesDownloaded * 100 / it.totalBytesToDownload).toInt()
+                // Set the progress for the progress bar.
+                //progressBar.setProgress(progress)
+                println(progress)
+                if(progress == 100)
+                {
+                    Log.d("API_TEST", "Modulo Instalado")
+
+                }
+            }
+
+            if (isTerminateState(update.installState)) {
+                //moduleInstallClient.unregisterListener(this)
+            }
+        }
+
+        fun isTerminateState(@ModuleInstallStatusUpdate.InstallState state: Int): Boolean {
+            return state == STATE_CANCELED || state == STATE_COMPLETED || state == STATE_FAILED
+        }
+    }
+
+    val listener = ModuleInstallProgressListener()
+
+
+
 }
