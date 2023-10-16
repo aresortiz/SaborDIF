0f8d31c9c432620ff4256b7a4cbae101a2c7f487
A01798012 <a01798012@tec.mx>
2023-10-16 17:47:35 -0600

    movimiento entre comidas

--- SaborDIF App/SaborDIFApp/app/src/main/java/com/example/sabordifapp/view/OpcionesComida.kt
+++ SaborDIF App/SaborDIFApp/app/src/main/java/com/example/sabordifapp/view/OpcionesComida.kt
@@ -6,28 +6,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModel
+import androidx.navigation.fragment.findNavController
 import com.example.sabordifapp.viewmodel.OpcionesComidaViewModel
 import com.example.sabordifapp.R
+import com.example.sabordifapp.databinding.FragmentComidaAdicionalBinding
+import com.example.sabordifapp.databinding.FragmentOpcionesComidaBinding
+import com.example.sabordifapp.viewmodel.APIVM.viewmodel.ComidaVM
 
 class OpcionesComida : Fragment() {
 
+    private lateinit var binding: FragmentOpcionesComidaBinding
     companion object {
         fun newInstance() = OpcionesComida()
     }
-
-    private lateinit var viewModel: OpcionesComidaViewModel
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_opciones_comida, container, false)
+        binding = FragmentOpcionesComidaBinding.inflate(layoutInflater)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(OpcionesComidaViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        registrarComidaComensal()
+        registrarComidaDependiente()
+    }
+
+    private fun registrarComidaDependiente(){
+        binding.btnComidaDependiente.setOnClickListener {
+            val accion = OpcionesComidaDirections.actionOpcionesComidaToComidaAdicional()
+            findNavController().navigate(accion)
+        }
+    }
+    private fun registrarComidaComensal(){
+        binding.btnComidaComensal.setOnClickListener {
+            val accion = OpcionesComidaDirections.actionOpcionesComidaToComida()
+            findNavController().navigate(accion)
+        }
     }
 
 }
